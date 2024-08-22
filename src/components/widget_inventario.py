70d42d96ba3a0fb9bd13d525e8aaf8f42ced9d5e
diogenes69 <diogenes_monegro@claro.com.do>
2024-08-22 00:35:27 -0400

    feat: add many field and add clotch

--- src/components/widget_inventario.py
+++ src/components/widget_inventario.py
@@ -5,12 +5,14 @@ from PySide6.QtWidgets import (
     QLabel,
     QPushButton,
     QComboBox,
-    QTableWidget,
-    QTableView
+    QHBoxLayout,
+    QLineEdit,
+    QTableView,
+    QTableWidget
 )
 
 from src.db.service_inventario import get_all_ropa
-from src.db.model import Marca
+from src.db.model import Marca, Ropa
 from src.db.model import Size
 from src.db.model import Categoria
 from src.components.widget_table import TableModel
@@ -26,6 +28,9 @@ class WidgetInventario(QWidget):
         self.__load_data()
 
     def __init_components(self):
+        self.layout_buttons = QHBoxLayout()
+        self.layout_buttons.setAlignment(Qt.AlignTop)
+       
         label = QLabel("Inventario")
         self.layout.addWidget(label)
 
@@ -55,18 +60,45 @@ class WidgetInventario(QWidget):
         self.layout.addWidget(select)
         self.layout.addWidget(self.combo_size)
 
-        button = QPushButton("Realizar venta")
-        self.layout.addWidget(button)
+        precio = QLabel("Precio")
+        self.q_precio = QLineEdit()
+        self.layout.addWidget(self.q_precio)
+        self.layout.addWidget(precio)
+
+        cantidad = QLabel("Cantidad")
+        self.q_cantidad = QLineEdit()
+        self.layout.addWidget(self.q_cantidad)
+        self.layout.addWidget(cantidad)
+
+
+        button = QPushButton("Agregar", clicked=self.__agregar)
+        self.layout_buttons.addWidget(button)
+        button = QPushButton("Eliminar")
+        self.layout_buttons.addWidget(button)
+        button = QPushButton("Modificar")
+        self.layout_buttons.addWidget(button)
+    
+        self.layout.addLayout(self.layout_buttons)
 
         self.table = QTableView()
         self.table.horizontalHeader().setStretchLastSection(True)
         self.table.setSelectionBehavior(QTableWidget.SelectRows)
         self.layout.addWidget(self.table)
 
+    def __agregar(self):
+        marca = Marca.get(Marca.nombre == self.combo.currentText())
+        categoria = Categoria.get(Categoria.nombre == self.combo_categoria.currentText())
+        size = Size.get(Size.nombre == self.combo_size.currentText())
+        cantidad = int(self.q_cantidad.text())
+        precio = float(self.q_precio.text())
+        ropa = Ropa.create(precio=precio, categoria=categoria.id, size=size.id, marca=marca.id, cantidad=cantidad)
+        ropa.save()
+        self.__load_data()
     def __load_data(self):
 
         ropas = get_all_ropa()
-        headers = ["ID", "", "Precio", "Categoria", "Size", "Marca", "Cantidad"]
-        values = ["id", "", "precio", "categoria", "size", "marca", "cantidad"]
+        print(ropas)
+        headers = ["ID", "Precio", "Categoria", "Size", "Marca", "Cantidad"]
+        values = ["id", "precio", "categoria", "size", "marca", "cantidad"]
         tablemodel = TableModel(ropas, headers, values)
         self.table.setModel(tablemodel)
